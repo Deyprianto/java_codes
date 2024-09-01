8f0f6bcad0037ff73ea9fb3d1d46a3e6a3a78c0b
prianto dey <149568690+Deyprianto@users.noreply.github.com>
2024-09-01 19:00:14 +0600

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Biodata{
+    public static void main(String[] args)
+   { 
+    Scanner input = new Scanner(System.in); 
+    String x;
+    System.out.print("Who is the prettiest girl in the world ?");
+    x = input.nextLine();
+   System.out.print("Yes you are correct she is the prittiest girl and she is no other than"+x);
+}
+}
